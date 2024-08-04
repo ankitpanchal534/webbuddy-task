5b168b78a99db49fcc404a8fd3c55e9d928885b1
ankitpanchal534 <ankitpanchal534@gmail.com>
2024-08-04 16:19:59 +0530

    added dnd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GlobeIcon from "./icons/GlobeIcon";
 import { features } from "./utils/features_data";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function Page() {
   return (
@@ -32,11 +33,12 @@ export default function Page() {
           backgroundRepeat: "no-repeat",
         }}
       />
-      <div className="relative z-0">
-        <NavBar />
+      <div className="relative pb-96 z-0">
         <Header />
-
         <InteractiveElement />
+      </div>
+      <div className="z-50 relative">
+        <NavBar />
         <LovedBySection />
       </div>
     </div>
@@ -44,7 +46,7 @@ export default function Page() {
 }
 
 const NavBar = () => (
-  <nav className=" text-sm flex justify-center fixed bottom-5 left-0 w-full text-black font-medium z-10">
+  <nav className=" text-sm flex justify-center  w-full text-black font-medium z-[500]">
     <div className="container bg-white xl:max-w-screen-lg 3xl:max-w-screen-xl shadow-[0px_0px_24px_lightgray] rounded-xl  px-4 py-2 flex justify-between items-center ">
       <div className="flex space-x-7 2xl:space-x-8 items-center">
         <button className="default-btn">Home</button>
@@ -97,60 +99,90 @@ const Header = () => (
 const InteractiveElement = () => {
   const [animate, setAnimate] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimate(true);
+    }, 1000);
+  }, []);
+
+  const onDragEnd = (result: any) => {
+    console.log(result);
+  };
+
   return (
-    <div className="absolute top-0 left- pb-32 w-full justify-center ">
-      <div className="container  mt-96 mx-auto 2xl:max-w-xl lg:max-w-lg px-4 py-8 text-center drop-box">
-        {/* <img
-        src="/icons/ai-icon.png"
-        className="h-16 w-16 rounded-full m-auto animate-spin select-none"
-        alt="ai icon"
-      /> */}
-        <div
-          style={{
-            backgroundImage: "url(/icons/ai-icon.png)",
-            backgroundSize: "100%",
-            backgroundRepeat: "no-repeat",
-          }}
-          className="h-16 w-16 rounded-full m-auto animate-spin select-none"
-        />
-        <p className="text-sm font-bold  mt-2">Drag features to view details</p>
-        {/* interactive element here */}
-      </div>
-      <div className="absolute top-0 h-full w-full  ">
-        <button
-          className="absolute bottom-0 left-1/2 -translate-x-28 "
-          onClick={() => setAnimate(true)}
-        >
-          <img src="/icons/Group 14.svg" alt="svg icon" />
-        </button>
-        {features.map((it, index) => {
-          return (
-            <button
-              key={it.id}
-              className={`absolute duration-500`} // style={{
-              //   top: it.top,
-              // }}
-              // className={`absolute top-[0px] left-[${it.left}px]  `}
-              style={{
-                left: `${(index + 1) * 120}px`,
-                top: animate
-                  ? `${(index < 5 ? index + 1 : 5 - index + 1) * 120}px`
-                  : "100%",
-              }}
-            >
-              {it.top}
-              {50 * (index + 1)}
-              <img src={`/icons/${it.icon}`} alt="svg icon" />
-            </button>
-          );
-        })}
-      </div>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="tasks">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {provided.placeholder}
+            <div className="absolute top-0 left-0 min-h-[800px] w-full justify-center ">
+              <div className="container  mt-96 mx-auto 2xl:max-w-xl lg:max-w-lg px-4 py-8 text-center drop-box">
+                <div
+                  style={{
+                    backgroundImage: "url(/icons/ai-icon.png)",
+                    backgroundSize: "100%",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                  className="h-16 w-16 rounded-full m-auto animate-spin select-none"
+                />
+                <p className="text-sm font-bold  mt-2">
+                  Drag features to view details
+                </p>
+                {/* interactive element here */}
+                {/* </div> */}
+              </div>
+            </div>
+
+            <div className="absolute top-0 h-full w-full flex justify-center ">
+              <button
+                className="absolute bottom-32 left-1/2 -translate-x-20 z-[11]"
+                // onClick={() => setAnimate(true)}
+              >
+                <img src="/icons/Group 14.svg" alt="svg icon" />
+              </button>
+              <div className=" relative z-20 h-full w-full 2xl:max-w-screen-xl">
+                {features.map((it, index) => {
+                  return (
+                    <Draggable
+                      key={it.id}
+                      draggableId={it.id.toString()}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          <button
+                            key={it.id}
+                            className={`absolute duration-500 z-[10] ${
+                              animate ? "" : "-translate-x-28"
+                            }`}
+                            style={{
+                              left: animate ? `${it.left}px` : "50%",
+                              top: animate ? `${it.top}px` : "83%",
+                              opacity: animate ? "100%" : "0%",
+                            }}
+                          >
+                            <img src={`/icons/${it.icon}`} alt="svg icon" />
+                          </button>
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
 const LovedBySection = () => (
-  <section className=" py-20 mt-52 z-[1]">
+  <section className=" py-20  z-[1]">
     <div className="container mx-auto px-4 text-center">
       <h2 className="text-[16px] text-[#171618] font-medium">
         Loved by product folks at
